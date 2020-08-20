ddfbcd48cdf726ae6c73cac0ba28ede6e8f4dc00
karolis <karolis.stulgys@gmail.com>
2020-08-20 22:20:24 +0300

    wip

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,10 +7,20 @@ export default function Layout({ children }) {
       <Head>
         {/* <meta name="theme-color" content='black'/> */}
         <title>LiftingBull | App</title>
+        <meta name="application-name" content="Lifting Bull" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Lifting Bull" />
+        <meta name="description" content="Lifting Bull" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/images/pwa-192.png" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
       <Stack w="full" spacing="0" bg="gray.900" fontFamily="Montserrat">
         {children}
--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
-// eslint-disable-next-line no-undef
 module.exports = withPWA({
   pwa: {
-    disable: !isProd,
     dest: 'public',
+    runtimeCaching,
   },
 })
