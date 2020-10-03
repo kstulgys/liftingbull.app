59c3918b87912e722e66dadf5d3f4a74d4738345
karolis <karolis.stulgys@gmail.com>
2020-10-03 22:05:11 +0300

    add buy me a coffee

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,23 +5,20 @@ export default function Layout({ children }) {
   return (
     <Box>
       <Head>
-        {/* <meta name="theme-color" content='black'/> */}
         <title>LiftingBull | App</title>
-        {/* <meta name="application-name" content="Lifting Bull" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Lifting Bull" />
-        <meta name="description" content="Lifting Bull" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="theme-color" content="#FFFFFF" /> */}
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover" />
-
-        {/* <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="shortcut icon" href="/icons/favicon.ico" /> */}
-        {/* <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" /> */}
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet" />
+        <script
+          data-name="BMC-Widget"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="kstulgys"
+          data-description="Support me on Buy me a coffee!"
+          data-message="Thank you for visiting. You can now buy me a coffee!"
+          data-color="#39B2AB"
+          data-position=""
+          data-x_margin="18"
+          data-y_margin="18"
+        />
       </Head>
       <Stack width="full" minH="100vh" height="full" spacing="0" bg="gray.900" fontFamily="Ubuntu">
         {children}
